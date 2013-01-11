2abb56051285199dda83380399652b5a366afb6d
Cory Thompson <coryjthompson@gmail.com>
2013-01-11 19:25:33 +1100

    added valid api key and spam tests

--- Test/Case/Lib/AkismetTest.php
+++ Test/Case/Lib/AkismetTest.php
@@ -1,4 +1,5 @@
 <?php
+
 App::uses('Akismet', 'Akismet.Lib');
 
 class AkismetTest extends CakeTestCase {
@@ -13,11 +14,28 @@ class AkismetTest extends CakeTestCase {
 		} catch(Exception $e){
 			$this->assertEqual("Akismet API returned invalid. Check API key", $e->getMessage());
 		}
+	}
 
+	public function testValidApiKey(){
+		//only valid if Akismet/Config/config.php is correct
+		Configure::load('Akismet.config');
+		$settings = Configure::read('akismet');
+		$this->akismetObj = new Akismet($settings['key'], $settings['blog']);
 	}
 
 	public function testSpam(){
-		return false;
+		$comment = array(
+				'user_ip' => '127.0.0.1',
+				'user_agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.101 Safari/537.11',
+				'comment_author' => 'viagra-test-123',
+				'comment_author_email' => 'viagra@viagra.com',
+				'comment_author_url' => 'buymypillypilles.com',
+				'comment_content' => 'spam spam spam'
+			);
+
+		$akismetObj = new Akismet(Configure::read('akismet.key'), Configure::read('akismet.blog'));
+		$result = $akismetObj->isSpam($comment);
+		$this->assertTrue($result);			
 	}
 
 }
